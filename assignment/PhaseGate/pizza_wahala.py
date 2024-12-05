42dd57c0cc38860a4dfc30fda854e11da64afd73
favour-rite <figwe000@gmail.com>
2024-12-05 08:16:24 +0100

    update

--- assignment/PhaseGate/pizza_wahala.py
+++ assignment/PhaseGate/pizza_wahala.py
@@ -1,31 +1,33 @@
-import string
+import math
+
 guests = int(input("Enter number of guests that will attend the birthday party: "))
-pizza = (input("sapa size, small money, big boys, odogwu "))
+pizza_sizes = (input("sapa size, small money, big boys, odogwu "))
 
 
 price_per_box = 0
 number_of_slices = 0
+
 if pizza == "sapa size":
 	number_of_slices = 4
-	price_per_box = 2000
+	prices_per_box = 2000
 
 elif pizza == "small money":
 	number_of_slices = 6
-	price_per_box = 2400
+	prices_per_box = 2400
 
 elif pizza == "big boys":
 	number_of_slices = 8
-	price_per_box = 3000
+	prices_per_box = 3000
 
 elif pizza == "odogwu":
 	number_of_slices = 12
-	price_per_box = 4200
+	prices_per_box = 4200
 
-boxes_of_pizza = (guests / number_of_slices)+1;
+number_of_boxes = math.ceil()
 
-total_slice = boxes_of_pizza * number_of_slices;
-left_over = total_slice - guests;
-total_price = price_per_box * boxes_of_pizza;
+
+
+
 
 print("Number of pizza to buy: " , boxes_of_pizza , " boxes")
 print("Number of left_over: " , left_over , " slices")
